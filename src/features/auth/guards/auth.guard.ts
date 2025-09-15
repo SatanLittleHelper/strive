3d3f79388642306ce409581866ef2d221beac633
Aleksandr Fenin <madhamst3r@gmail.com>
2025-09-15 20:08:40 +0300

    feature/automatic-token-refresh: fix token validation logic to allow single valid token

--- src/features/auth/guards/auth.guard.ts
+++ src/features/auth/guards/auth.guard.ts
@@ -2,16 +2,18 @@ import { inject } from '@angular/core';
 import { Router, type CanMatchFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanMatchFn = (): boolean => {
+export const authGuard: CanMatchFn = async (): Promise<boolean> => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (!authService.isAuthenticated()) {
+  debugger;
+  const isAuthenticated = await authService.isAuthenticatedAndValid();
+
+  if (!isAuthenticated) {
     const url = router.url;
     if (url !== '/login' && url !== '/register') {
       sessionStorage.setItem('return_url', url);
     }
-
     void router.navigate(['/login']);
     return false;
   }
